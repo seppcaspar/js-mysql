881efeb61fecd1a57c78c2bd8e4bb243505101d2
caspar.sepp <caspar.sepp@tptlive.ee>
2023-11-15 11:24:42 +0200

    quick bug fix

---
+++ nignog.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql2');
+
+
+// create a new MySQL connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'Passw0rd',
+  database: 'autism'
+});
+// connect to the MySQL database
+connection.connect((error) => {
+  if (error) {
+    console.error('Error connecting to MySQL database:', error);
+  } else {
+    console.log('Connected to MySQL database!');
+  }
+});
+// close the MySQL connection
+connection.end();
